6193e26bfa1da5f065e50ecba1895423af98d438
CrazyWonton <kevintarc13@gmail.com>
2017-11-11 18:07:43 -0500

    Reads input from file and puts into priority queue
    Added additional functionalities to program. ReadInStuff now correctly
    reads in data from file and puts it into a priority queue for later use
    in Best First Search algorithm

--- BestFirstSearch.java
+++ BestFirstSearch.java
@@ -1,32 +1,74 @@
 import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class BestFirstSearch{
-static int n = 0;
-static int C = 0;
+	public static class Pair{
+		int weight;
+		int cost;
+		int ratio;
 
-	void readInStuff(string filename) {
-		String line = null;
-		try {
-			FileReader fileReader = new FileReader(fileName);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			line = bufferedReader.readLine();
-			n = line.parseInt();
-			int commaIndex = line.indexOf(',');
-			C = line.substring(commaIndex).parseInt();
-			
-			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line);
-			}
-			bufferedReader.close();
-		} catch (FileNotFoundException ex) {
-			System.out.println("Unable to open file '" + fileName + "'");
-		} catch (IOException ex) {
-			System.out.println("Error reading file '" + fileName + "'");
+		public Pair(int w, int c){
+			weight = w;
+			cost = c;
+			ratio = c/w;
+		}
+
+		public String toString(){
+			return "[" + weight + ", " + cost + "] = " + ratio;
 		}
 	}
 
-	public static void main(String[] args) {
-		readInStuff(args[1]);
-		System.out.println("Hello world");
+	public static class PairComparator implements Comparator<Pair>{
+    @Override
+    public int compare(Pair x, Pair y){
+        if (x.ratio > y.ratio)
+            return -1;
+        if (x.ratio < y.ratio)
+            return 1;
+        return 0;
+    }
 	}
+
+
+	static int n = 0;
+	static int C = 0;
+	static Comparator<Pair> comparator;
+	static PriorityQueue<Pair> pQueue;
+
+	static void readInStuff(String filename) {
+		String line = null;
+			int commaIndex = 0;
+			int cost = 0;
+			int weight = 0;
+			try {
+				FileReader fileReader = new FileReader(filename);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				line = bufferedReader.readLine();
+				commaIndex = line.indexOf(',');
+				n = Integer.parseInt(line.substring(0,commaIndex).trim());
+				C = Integer.parseInt(line.substring(commaIndex+1).trim());
+				for(int i=0;i<n;i++){
+					line = bufferedReader.readLine();
+					commaIndex = line.indexOf(',');
+					weight = Integer.parseInt(line.substring(0,commaIndex).trim());
+					cost = Integer.parseInt(line.substring(commaIndex+1).trim());
+					Pair temp = new Pair(weight, cost);
+					pQueue.add(temp);
+				}
+				bufferedReader.close();
+			} catch (FileNotFoundException ex) {
+				System.out.println("Unable to open file '" + filename + "'");
+			} catch (IOException ex) {
+				System.out.println("Error reading file '" + filename + "'");
+			}
+		}
+
+		public static void main(String[] args) {
+			comparator = new PairComparator();
+			pQueue = new PriorityQueue<Pair>(comparator);
+			readInStuff(args[0]);
+			while (pQueue.size() != 0)
+	      System.out.println(pQueue.remove());
+		}
 }
